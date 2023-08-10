1e2d61fbe49ac27e067e6ce3a6d52b3fd69bf00e
Arden Choi <ardenyschoi@gmail.com>
2023-08-10 12:54:21 -0700

    fixed navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import styles from "@/styles/Navbar.module.css";
+import NavItem from "./NavItem";
 
 const MENU_LIST = [
     { text: "Home", href: "/#hero" },
@@ -12,28 +13,31 @@ const MENU_LIST = [
     { text: "Contact", href: "/#contact" },
 ];
 
-const NavItem = ({ text, href, active }) => {
-  const handleClick = (e) => {
-    if (href.startsWith("/#")) { // Check if the link is an internal anchor
-      e.preventDefault();
-      const targetElement = document.querySelector(href.substring(1));
-      if (targetElement) {
-        targetElement.scrollIntoView({ behavior: 'smooth' });
-      }
-    }
-  };
+// const NavItem = ({ text, href, active }) => {
+//   const handleClick = (e) => {
+//     if (href.startsWith("/#")) { // Check if the link is an internal anchor
+//       e.preventDefault();
+//       const targetElement = document.querySelector(href.substring(1));
+//       if (targetElement) {
+//         targetElement.scrollIntoView({ behavior: 'smooth' });
+//       }
+//     }
+//   };
 
-  return (
-    <a href={href} onClick={handleClick} className={`${styles.navLink} ${active ? styles.active : ""}`}>
-      {text}
-    </a>
-  );
-  // return (
-  //   <Link href={href} legacyBehavior>
-  //     <a className={`${styles.navLink} ${active ? styles.active : ""}`}>{text}</a>
-  //   </Link>
-  // );
-};
+//   return (
+//     // <a href={href} onClick={handleClick} className={`${styles.navLink} ${active ? styles.active : ""}`}>
+//     //   {text}
+//     // </a>
+//     <Link href={href} legacyBehavior>
+//       <a onClick={handleClick} className={`${styles.navLink} ${active ? styles.active : ""}`}>{text}</a>
+//     </Link>
+//   );
+//   // return (
+//   //   <Link href={href} legacyBehavior>
+//   //     <a className={`${styles.navLink} ${active ? styles.active : ""}`}>{text}</a>
+//   //   </Link>
+//   // );
+// };
 
 const Navbar = () => {
   const [navActive, setNavActive] = useState(null);
