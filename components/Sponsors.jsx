d9e0cc89a68b64623e2ccc653aec47fe272dd7b8
Arden Choi <ardenyschoi@gmail.com>
2023-08-15 21:30:08 -0700

    fix veiwport

--- components/Sponsors.jsx
+++ components/Sponsors.jsx
@@ -4,7 +4,7 @@ import Carousel from './Carousel'
 export default function Sponsors() {
   return (
     <div id="sponsors" className="flex flex-col items-center justify-center min-h-screen">
-        <p class="text-2xl text-center ...">Sponsors!!</p>
+        <p class="text-2xl text-center ...">Sponsors</p>
         <Carousel />
     </div>
   )
