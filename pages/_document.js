8bf0bdde0d4bc5dc538a6cf7b85209561b44dc07
Arden Choi <ardenyschoi@gmail.com>
2023-09-16 22:56:58 -0700

    google analytics

--- pages/_document.js
+++ pages/_document.js
@@ -1,22 +1,25 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
+  const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID;
   return (
     <Html lang="en">
       <Head>
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-S7YGDXDCCS"></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-S7YGDXDCCS');
-            `,
-          }}
-        />
-        {/* <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet" /> */}
-        {/* <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1, maximum-scale=1, user-scalable=0"/> */}
+        {GA_MEASUREMENT_ID && (
+            <>
+              <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}></script>
+              <script
+                dangerouslySetInnerHTML={{
+                  __html: `
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${GA_MEASUREMENT_ID}');
+                  `,
+                }}
+              />
+            </>
+          )}
       </Head>
       <body>
         <Main />
