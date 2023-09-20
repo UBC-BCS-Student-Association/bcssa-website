a8478393d4876babc8e539aae4037305823cb8f7
Arden Choi <ardenyschoi@gmail.com>
2023-09-20 13:25:12 -0700

    vercel analytics

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Toaster } from "./ui/toaster";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function Layout({ children }) {
   return (
@@ -10,6 +11,7 @@ export default function Layout({ children }) {
       <main>{children}</main>
       <Footer />
       <Toaster />
+      <Analytics />
     </>
   );
 }
