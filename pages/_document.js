2087353f8ad7735d2d859803dc097f637cf70546
Arden Choi <ardenyschoi@gmail.com>
2023-09-30 22:24:53 -0700

    add favicon and disable google analytics

--- pages/_document.js
+++ pages/_document.js
@@ -1,7 +1,6 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
-  const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID;
   return (
     <Html lang="en">
       <Head>
@@ -11,7 +10,6 @@ export default function Document() {
           name="description"
           content="UBC Bachelor of Computer Science Student Association"
         />
-
         <meta property="og:title" content="UBC BCSSA" />
         <meta
           property="og:description"
@@ -21,23 +19,27 @@ export default function Document() {
         <meta property="og:url" content="https://www.ubcbcs.ca/" />
         <meta property="og:image" content="https://i.imgur.com/lJRkENI.jpg" />
 
-        {GA_MEASUREMENT_ID && (
-          <>
-            <script
-              async
-              src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}></script>
-            <script
-              dangerouslySetInnerHTML={{
-                __html: `
-                    window.dataLayer = window.dataLayer || [];
-                    function gtag(){dataLayer.push(arguments);}
-                    gtag('js', new Date());
-                    gtag('config', '${GA_MEASUREMENT_ID}');
-                  `,
-              }}
-            />
-          </>
-        )}
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#2b5797" />
+        <meta name="theme-color" content="#ffffff" />
       </Head>
       <body>
         <Main />
